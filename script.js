6af4172f87a7ef8d7f973c9322f88b9115f1fad4
leozanesco <zanesco.leo@gmail.com>
2022-08-25 18:07:15 -0300

    added excel4node and started building columns

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const axios = require("axios");
+const xl = require('excel4node');
 
 const API_URL = 'https://restcountries.com/v3.1/all';
+
+const wb = new xl.Workbook();
+const ws = wb.addWorksheet('Countries List');
+const EXCEL_COLUMNS = [
+    { key: 'name', value: 'Name'},
+    { key: 'capital', value: 'Capital'},
+    { key: 'area', value: 'Area'},
+    { key: 'currency', value: 'Currencies'},
+]
+
 
 const getCountriesData = async () => {
     try {
@@ -9,3 +20,4 @@ const getCountriesData = async () => {
         console.error(error)
     }
 }
+
