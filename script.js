da4bfaf9545732c09ce55cbbd0a3751f1da5829e
leozanesco <zanesco.leo@gmail.com>
2022-08-25 18:13:40 -0300

    Add writeExcel function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const xl = require('excel4node');
 
 const API_URL = 'https://restcountries.com/v3.1/all';
+const FILENAME = 'Excel.xlsx';
 
 const wb = new xl.Workbook();
 const ws = wb.addWorksheet('Countries List');
@@ -21,3 +22,29 @@ const getCountriesData = async () => {
     }
 }
 
+const writeExcel = async () => {
+    ws.cell(1, 1, 1, 4, true).string('Countries List')
+    EXCEL_COLUMNS.forEach(({ value }, columnIndex) => ws.cell(2, columnIndex +1).string(value))
+
+    const countriesData = await getCountriesData();
+    countriesData.data.forEach((element, elementIndex) => {
+        const row = {
+            name: element.name.common,
+            capital: element.capital ?? '-',
+            area: element.area ?? '-',
+            currency: element.currencies ? Object.keys(element.currencies).join(",") : '-',
+        };
+
+        EXCEL_COLUMNS.forEach(({ key }, columnIndex) => {
+            if (typeof row[key] === 'number'){
+                ws.cell(elementIndex + 3, columnIndex + 1).number(row[key] ? row[key] : 'N/A');
+            } else{
+                ws.cell(elementIndex + 3, columnIndex + 1).string(row[key] ? row[key] : 'N/A');
+            }
+
+    });
+
+    wb.write(FILENAME);
+})};
+
+writeExcel();
